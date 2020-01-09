AntonLydike/diggicamp

commit 8025f63a6183d242434ee01f696457fb36da85f1

fixed fetch error on files without a license

diggicamp/pages/course_files.py
@@ -82,6 +82,10 @@ def process_folder_async(diggicamp, course: str, folder: dict):
         if not link:
             raise Exception("link not found!")
 
+        if not 'href' in link:
+            # if file is not downloadable, skip it
+            continue
+
         fname = re.search(r'file_name=([^&]+)', link['href'])
 
         if not fname:

commit 91221571c5255eb8f162be5b46a51d4fd639fee5

fixed bug introduced in 8025f63a6183d242434ee01f696457fb36da85f1 that disables file listing

diggicamp/pages/course_files.py
@@ -82,7 +82,7 @@ def process_folder_async(diggicamp, course: str, folder: dict):
         if not link:
             raise Exception("link not found!")
 
-        if not 'href' in link:
+        if not 'href' in link.attrs:
             # if file is not downloadable, skip it
             continue
 