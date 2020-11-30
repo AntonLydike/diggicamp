8b80e990943724bd0bc273fbdaa22e92a79c86a4
Jonas Bühler <jonas.buehler@student.uni-augsburg.de>
2020-11-30 21:33:02 +0100

    Fixes for the new digicampus version
    In the new version some changes were made in the ui. Therefore some edits in the dom-parsing had to be made.

--- diggicamp/pages/course_files.py
+++ diggicamp/pages/course_files.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import threading
 import requests
 import re
-
+import os
 
 class CourseFiles:
     def __init__(self, diggicamp, courseId: str):
@@ -17,34 +17,17 @@ class CourseFiles:
         # list of threads
         threads = []
 
-        dom = ParsedPage(self.dgc._get(f'folder.php?cid={self.id}&cmd=tree')).dom
-
-        root = dom.find('div', id='folder_subfolders_root')
+        dom = ParsedPage(self.dgc._get(f'/dispatch.php/course/files?cid={self.id}&cmd=tree')).dom
 
         # initialize a asynchronous callback collection
         # everyone can call add_to_bag to add a return value to the collection
         (get_bag, add_to_bag) = create_bag()
-
-        for froot in root.children:
-            # what we want to know about this folder
-            id = None
-            if froot.div:
-                id = froot.div.string.strip()
-            else:
-                elem = froot.find(id=re.compile(r'folder_[A-f0-9]{32}_body'))
-                if elem:
-                    match = re.search(r'folder_([A-f0-9]{32})_body', elem['id'])
-                    if not match:
-                        raise Exception("id matches, but then does not match?!", elem.prettify())
-                    id = match.group(1)
-                else:
-                    print("no folder id discovered!\n" + froot.prettify())
-                    continue
-
-            name = unicode(" ".join(froot.find(id=f'folder_{id}_header').stripped_strings))
-
-            # start a thread to download folder contents
-            thread = threading.Thread(target=process_folder_async, args=(self.dgc, self.id, id, name, add_to_bag))
+        sub_folders = get_folders(dom)
+        if not sub_folders:
+            return
+        for folder in sub_folders:
+            # start a thread to download folder contents            
+            thread = threading.Thread(target=process_folder_async, args=(self.dgc, self.id, folder['id'], folder['name'], add_to_bag))
             thread.start()
             threads.append(thread)
 
@@ -59,12 +42,27 @@ class CourseFiles:
 
         return folders
 
+#returns all folders ((id, name) tuple) in the specified dom
+def get_folders(dom: str):
+    subfolders = dom.find('tbody', class_='subfolders').find_all(id=re.compile(r'row_folder_[A-f0-9]{32}'))
+    output_folders = []
+    for folder in subfolders:
+        id = folder['id'][11:]
+        name = folder.find_all('a')[1].text.strip()
+        #these characters are forbidden in directory names. thx windows...
+        name = name.translate(str.maketrans({"/":  "", "<":  "", ">": "", ":":  "", "\"":  "", "\\":  "", "?":  "", "*":  "", "|":  ""}))
+        output_folders.append({
+            'id': id,
+            'name': name,
+        })
+    return output_folders
+
 
 def process_folder_async(diggicamp, course: str, id: str, name: str, add_folder):
     # print a little progress indicator
     print('.', end='', flush=True)
 
-    html = diggicamp._post(f'/folder.php?cid={course}&data%5Bcmd%5D=tree&open={id}', data=f'getfolderbody={id}')
+    html = diggicamp._post(f'/dispatch.php/course/files/index/{id}?cid={course}', data=f'getfolderbody={id}')
     dom = ParsedPage(html).dom
 
     folder = {
@@ -73,33 +71,27 @@ def process_folder_async(diggicamp, course: str, id: str, name: str, add_folder)
         'files': []
     }
 
-    fcontainer = dom.find(id=f'folder_{id}', class_='folder_container')
-
     # query subfolders
-    subfolders = dom.find(id=f'folder_subfolders_{id}', class_='folder_container')
-    #print("found " + str(len(subfolders.find_all('div', recoursive=False))))
+    subfolders = get_folders(dom)
     if subfolders:
-        for subfolder in subfolders.findChildren('div', recoursive=False):
-            idfield = subfolder.find(id=re.compile('^getmd5'))
-            if not idfield:
-                continue  # this happens quite often and is not a sign of failure on our side, rather digicampus being derpy
-            subid = unicode(" ".join(idfield.stripped_strings))
-            subname = unicode(" ".join(subfolder.find(id=f'folder_{subid}_header').stripped_strings))
-            process_folder_async(diggicamp, course, subid, subname, add_folder)
+        for subfolder in subfolders:
+            process_folder_async(diggicamp, course, subfolder['id'], os.path.join(name, subfolder['name']), add_folder)
+
+    fcontainer = dom.find('tbody', class_='files').find_all(id=re.compile(r'fileref_[A-f0-9]{32}'))
 
     if not fcontainer:
         return
 
-    for file in list(fcontainer.children):
+    for file in fcontainer:
         if isinstance(file, NavigableString):
             continue
 
-        if file.div:
-            id = file.div.string.strip()
+        if file:
+            id = file['id'][8:]
         else:
             raise Exception("No id for file found!", file.prettify())
 
-        link = file.find(id=f'file_{id}_header').parent.previous_sibling
+        link = file.find_all('td')[2].find('a')
 
         if not link:
             raise Exception("link not found!")
@@ -107,35 +99,22 @@ def process_folder_async(diggicamp, course: str, id: str, name: str, add_folder)
         if not 'href' in link.attrs:
             # if file is not downloadable, skip it
             continue
-
-        fname = re.search(r'file_name=([^&]+)', link['href'])
-        ftype = re.search(r'type=([0-9]+)', link['href'])
-
-        if not fname:
-            raise Exception('link url has invalid format')
-
-        if not ftype:
-            print("no download type found: " + link.prettify())
-            ftype = 0
-        else:
-            ftype = ftype.group(1)
         
-
-        fname = requests.utils.unquote(fname.group(1), encoding="1250")
-
-        last_modified_elm = file.find('td', class_='printhead', align='right')
+        fname = link.text.strip()
+        if not fname:
+            raise Exception("filename not found!")
+        
+        last_modified_elm = file.find_all('td')[5]['title']
         if last_modified_elm:
-            if last_modified_elm.a:
-                last_modified_elm.a.extract()
-            last_modified = last_modified_elm.string.strip()
+            last_modified = last_modified_elm.strip()
         else:
             last_modified = str(datetime.now())
 
         folder['files'].append({
             'id': id,
-            'name': unicode(" ".join(file.find(id=f'file_{id}_header').stripped_strings)),
+            'name': fname,
             'fname': fname,
-            'type': ftype,
+            'type': 0,
             'last_mod': last_modified
         })
         print('.', end='', flush=True)
