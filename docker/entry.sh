b485ee35e9d36a35af6db86d2dce2963ca8325c7
Tobias Kiehnlein <tobias@kiehnlein.com>
2022-05-22 20:56:35 +0200

    First working docker configuration with some sort of user support

--- docker/entry.sh
+++ docker/entry.sh
@@ -1,26 +1,8 @@
 #!/usr/bin/env bash
 
-ls -lha ./
-ls -lha /
-ls -lha /diggicamp
+chown -R $FILES_USER:$FILES_GROUP /data
 
-FILE=/config/dgc.json
-if ! test -f "$FILE"; then
-  echo Initializing dgc...
-  dgc init "$URL" --user "$USER" --pass "$PASS";
-fi
-
-RESULT=$(dgc show)
-RAW_SEMESTER=$(echo "$RESULT" | grep -E "[WS]S.*:")
-SEMESTER=${RAW_SEMESTER/":"/""}
-SEMESTER=${SEMESTER/"/"/"_"}
-SEMESTER=$(echo "$SEMESTER" | sed -e 's/\s$//g')
-SEMESTER=$(echo "$SEMESTER" | sed -e 's/\s/_/g')
-RAW_COURSES=$(echo "$RESULT" | grep -E "^\s*-.*$")
-COURSES=$(echo "$RAW_COURSES" | sed -e 's/^\s*-\s*//g')
-while IFS= read -r COURSE; do
-  echo "Handling course $COURSE"
-  dgc downloads add "$COURSE" "/downloads/$SEMESTER/$COURSE" --regex "$REGEX"
-done <<< "$COURSES"
-
-dgc pull;
+while :; do
+  dgc-timer
+  sleep 1m
+done
